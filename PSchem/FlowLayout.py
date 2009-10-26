71aaac90ebe315f02467fcbdeaa70681a5543ccd
andrzej-r <ndrwrdck@gmail.com>
2009-10-26 14:04:26 +0000

    Added license information and more clean-up.
    git-svn-id: http://pschem.googlecode.com/svn/trunk@5 63eb347a-c228-11de-acf9-4f1248423c03

--- PSchem/FlowLayout.py
+++ PSchem/FlowLayout.py
@@ -1,9 +1,66 @@
 # -*- coding: utf-8 -*-
 from PyQt4 import QtCore, QtGui
 
-# Port of a FlowLayout Qt example
-# (with some extensions)
+# This file is part of PSchem.
+ 
+# PSchem is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 
+# PSchem is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with PSchem.  If not, see <http://www.gnu.org/licenses/>.
+
+# This file was ported from a FlowLayout Qt example.
+
+# Original notice:
+# /****************************************************************************
+# **
+# ** Copyright (C) 2009 Nokia Corporation and/or its subsidiary(-ies).
+# ** All rights reserved.
+# ** Contact: Nokia Corporation (qt-info@nokia.com)
+# **
+# ** This file is part of the examples of the Qt Toolkit.
+# **
+# ** $QT_BEGIN_LICENSE:LGPL$
+# ** Commercial Usage
+# ** Licensees holding valid Qt Commercial licenses may use this file in
+# ** accordance with the Qt Commercial License Agreement provided with the
+# ** Software or, alternatively, in accordance with the terms contained in
+# ** a written agreement between you and Nokia.
+# **
+# ** GNU Lesser General Public License Usage
+# ** Alternatively, this file may be used under the terms of the GNU Lesser
+# ** General Public License version 2.1 as published by the Free Software
+# ** Foundation and appearing in the file LICENSE.LGPL included in the
+# ** packaging of this file.  Please review the following information to
+# ** ensure the GNU Lesser General Public License version 2.1 requirements
+# ** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+# **
+# ** In addition, as a special exception, Nokia gives you certain additional
+# ** rights.  These rights are described in the Nokia Qt LGPL Exception
+# ** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+# **
+# ** GNU General Public License Usage
+# ** Alternatively, this file may be used under the terms of the GNU
+# ** General Public License version 3.0 as published by the Free Software
+# ** Foundation and appearing in the file LICENSE.GPL included in the
+# ** packaging of this file.  Please review the following information to
+# ** ensure the GNU General Public License version 3.0 requirements will be
+# ** met: http://www.gnu.org/copyleft/gpl.html.
+# **
+# ** If you have questions regarding the use of this file, please contact
+# ** Nokia at qt-info@nokia.com.
+# ** $QT_END_LICENSE$
+# **
+# ****************************************************************************/
+
+ 
 class FlowLayout(QtGui.QLayout):
     def __init__(self, parent=None, margin=None, spacing=None):
         QtGui.QLayout.__init__(self, parent)
--- pschem.bat
+++
@@ -1,2 +0,0 @@
-﻿
-C:\Python26\pythonw.exe "C:\Documents and Settings\andrzejr\My Documents\Design\pschem\pschem.pyw"
